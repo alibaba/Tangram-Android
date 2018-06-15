3cdf3985f9e5d6885f4f33abb686c4eb52c0a65c
MikeAfc <mike8264791@gmail.com>
2018-06-15 14:45:07 +0800

    Add real-time template for tangram

---
+++ tangram/src/main/java/com/tmall/wireless/tangram/support/TemplateUpdateSupport.java
@@ -0,0 +1,9 @@
+package com.tmall.wireless.tangram.support;
+
+import com.tmall.wireless.tangram.structure.TemplateInfo;
+
+public class TemplateUpdateSupport {
+    public void onUpdate(TemplateInfo templateInfo) {
+
+    }
+}
